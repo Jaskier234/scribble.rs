d8866fa6381bf88612070e860699722da439628f
jaskier234 <jaskier234@gmail.com>
2021-02-20 15:53:26 +0100

    Only one random word

--- game/lobby.go
+++ game/lobby.go
@@ -532,7 +532,7 @@ func advanceLobby(lobby *Lobby) {
 	lobby.drawer = newDrawer
 	lobby.drawer.State = Drawing
 	lobby.state = ongoing
-	lobby.wordChoice = GetRandomWords(3, lobby)
+	lobby.wordChoice = GetRandomWords(1, lobby)
 
 	recalculateRanks(lobby)
 
